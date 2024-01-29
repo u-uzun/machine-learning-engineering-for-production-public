e45a875654a1ebe1451762f7a67c06cd426bd703
u-ufuk <ufuk.uzun@gmail.com>
2024-01-29 19:00:32 +0300

    Adding new classifier with scaling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -3,15 +3,28 @@ import numpy as np
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 # Test comment line
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine-95.pkl", "rb") as file:
+with open("models/wine-95-fixed.pkl", "rb") as file:
     clf = pickle.load(file)
 
+def test_pipeline_and_scaler():
+
+    # Check if clf is an instance of sklearn.pipeline.Pipeline
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+
+    if isPipeline:
+        # Check if first step of pipeline is an instance of
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
 
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_length=13, max_length=13)]
